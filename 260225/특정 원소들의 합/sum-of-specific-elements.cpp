2f19c3cd24b5a42e54879d52342239d89b1c9b4f
The_Normal_One <gdool88@gmail.com>
2026-02-25 22:37:09 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260225/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[4][4] = {{3,3,54,5},{2,6,7,81},{3,31,2,1},{95,5,7,1}};
+
+    int result = 0;
+    
+    for(int i=0; i<4;i++){
+        for(int j=0; j<=i; j++){
+            result += arr[i][j];
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
