c4820c17cf2356fec3ba27775f83fc8d1e5996fd
The_Normal_One <gdool88@gmail.com>
2026-02-25 22:38:43 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260225/특정 원소들의 합/sum-of-specific-elements.cpp
+++ 260225/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int arr[4][4] = {{3,3,54,5},{2,6,7,81},{3,31,2,1},{95,5,7,1}};
+    //int arr[4][4] = {{3,3,54,5},{2,6,7,81},{3,31,2,1},{95,5,7,1}};
+
+
+    int arr[4][4];
+
+    for(int i=0; i<4;i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+
 
     int result = 0;
     
