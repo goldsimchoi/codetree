14b7f85d0b22c3155d982d2b22f2662d6acdbeeb
The_Normal_One <gdool88@gmail.com>
2026-02-25 23:09:34 +0900

    Title: 아래로 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260225/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
+++ 260225/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -19,7 +19,7 @@ int main() {
 
     for(int i=0; i<n;i++){
         for(int j=0; j<n; j++){
-            cout << arr[j][i]; << ' ';
+            cout << arr[i][j] << ' ';
         }
         cout << '\n';
     }
