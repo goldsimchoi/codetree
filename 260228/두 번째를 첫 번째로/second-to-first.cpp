b13d884a4505da774ea37aebfe6495b0dec63b6f
The_Normal_One <gdool88@gmail.com>
2026-02-28 21:49:27 +0900

    Title: 두 번째를 첫 번째로, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260228/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string str;
+
+    cin >> str;
+    for(int i=0; i<str.length();i++){
+        if(str[i]==str[1]){
+            str[i]=str[0]
+        }
+    }
+
+    cout << str;
+
+    return 0;
+}
