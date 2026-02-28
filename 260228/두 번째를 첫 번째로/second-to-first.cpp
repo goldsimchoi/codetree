9d1ddfb191c117bece7a7b45f1927fc012b2c8d5
The_Normal_One <gdool88@gmail.com>
2026-02-28 21:50:26 +0900

    Title: 두 번째를 첫 번째로, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260228/두 번째를 첫 번째로/second-to-first.cpp
+++ 260228/두 번째를 첫 번째로/second-to-first.cpp
@@ -7,7 +7,7 @@ int main() {
     string str;
 
     cin >> str;
-    char tmp = str[1]
+    char tmp = str[1];
     for(int i=0; i<str.length();i++){
         if(str[i]==tmp){
             str[i]=str[0];
