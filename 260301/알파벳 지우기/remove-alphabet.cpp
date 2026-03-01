3a13652c41dd70261c9b448eff18bc12878742a4
The_Normal_One <gdool88@gmail.com>
2026-03-01 18:35:09 +0900

    Title: 알파벳 지우기, Time: 10ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260301/알파벳 지우기/remove-alphabet.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string s1,s2;
+
+    cin >> s1;
+    cin >> s2;
+    int idx=0;
+
+    while(idx < s1.length()){
+
+        if((int)s1[idx]>47 && (int)s1[idx]<58){
+            idx++;
+        }
+
+        else{
+            s1.erase(idx,1);
+        }
+
+    }
+
+
+    while(idx < s2.length()){
+
+        if((int)s2[idx]>47 && (int)s2[idx]<58){
+            idx++;
+        }
+
+        else{
+            s2.erase(idx,1);
+        }
+
+    }
+
+
+
+
+    cout << stoi(s1); + stoi(s2);
+
+    return 0;
+}
