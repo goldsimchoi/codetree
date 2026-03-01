7e26e38af7bfd64147beb21631b4c5b6e247535b
The_Normal_One <gdool88@gmail.com>
2026-03-02 00:21:58 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260302/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
+++ 260302/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -9,10 +9,7 @@ void square(int n,int m){
 
     for(int i=0; i<n;i++){
         for(int j=0; j<m;j++){
-            cout << 1 << ' ';
-
-
-        }
+            cout << 1;
         cout << '\n';
     }
 
