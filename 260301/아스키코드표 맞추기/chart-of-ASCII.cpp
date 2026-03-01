a2c1febe5a9095e66cfc26bfedbb5679d09dfdb7
The_Normal_One <gdool88@gmail.com>
2026-03-01 18:05:43 +0900

    Title: 아스키코드표 맞추기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260301/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c,d,e;
+
+    cin >> a >> b >> c >> d >> e;
+
+
+    cout << (char)a <<' '<< (char)b <<' '<<(char)c <<' '<<(char)d <<' '<<(char)e;
+
+
+    return 0;
+}
