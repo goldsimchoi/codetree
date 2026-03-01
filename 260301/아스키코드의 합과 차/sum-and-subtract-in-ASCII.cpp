44cec70c87532c327bb85469fefe3962f0faac91
The_Normal_One <gdool88@gmail.com>
2026-03-01 18:03:36 +0900

    Title: 아스키코드의 합과 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260301/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    char a,b;
+    cin >> a >> b;
+    cout << (int)a + (int)b<< ' ';
+    cout << abs((int)a - (int)b) << '\n';
+
+    
+    return 0;
+}
