2902857f7c48091ea3fd15b274b6300c3c304aec
The_Normal_One <gdool88@gmail.com>
2026-03-02 00:21:42 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260302/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+void square(int n,int m){
+
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<m;j++){
+            cout << 1 << ' ';
+
+
+        }
+        cout << '\n';
+    }
+
+}
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+
+    square(n,m);
+
+    return 0;
+}
