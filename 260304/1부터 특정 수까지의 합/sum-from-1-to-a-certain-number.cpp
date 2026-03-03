a3cfe734b86c02d369290d871703a4dd585660ee
The_Normal_One <gdool88@gmail.com>
2026-03-04 04:48:20 +0900

    Title: 1부터 특정 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260304/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void func(int n){
+    int sum=0;
+    for(int i=1;i<=n){
+        sum+=i;
+    }
+
+    cout << sum / 10;
+
+
+
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    func(N);
+    return 0;
+}
